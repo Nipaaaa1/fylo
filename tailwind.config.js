e3158294be3915ea5f86f5c842daf3b488e21a4e
Nipaaaa1 <Frostone224@gmail.com>
2024-05-10 15:07:28 +0700

    styles: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,22 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "dark-blue": "hsl(243, 87%, 12%)",
+          "desaturated-blue": "hsl(238, 22%, 44%)"
+        },
+        accent: {
+          "blue": "hsl(224, 93%, 58%)",
+          "cyan": "hsl(170, 45%, 43%)"
+        },
+        neutral: {
+          "blue": "hsl(240, 75%, 98%)",
+          "gray": "hsl(0, 0%, 75%)"
+        },
+      }
+    },
   },
   plugins: [],
 };
