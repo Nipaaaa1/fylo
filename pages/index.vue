e51002fa93fa30787df1be43e438e1b7fb006ddd
Nipaaaa1 <Frostone224@gmail.com>
2024-05-10 17:30:30 +0700

    feat(ui): add Get Started section

--- pages/index.vue
+++ pages/index.vue
@@ -79,4 +79,29 @@
       </div>
     </div>
   </section>
+  <section
+    class="container space-y-4 bg-primary-desaturated-blue px-6 py-16 text-center text-neutral-blue"
+  >
+    <h2 class="text-lg font-bold tracking-wider">Get early access today</h2>
+    <p>
+      It only takes a minute to sign up and our free starter tier is extremely
+      generous. If you have any questions, our support team would be happy to
+      help you.
+    </p>
+    <form class="mx-auto w-72 space-y-3">
+      <label for="email" class="sr-only w-0">Email</label>
+      <input
+        type="email"
+        name="email"
+        placeholder="email@example.com"
+        class="w-full rounded p-2 placeholder:font-open-sans placeholder:text-sm"
+      />
+      <button
+        type="submit"
+        class="w-full rounded bg-accent-blue p-2 text-sm font-bold tracking-wider text-white shadow-md"
+      >
+        Get Started
+      </button>
+    </form>
+  </section>
 </template>
