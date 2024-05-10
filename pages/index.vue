d62d2fee31d7758ab2a156c5be9f29f44bf93717
Nipaaaa1 <Frostone224@gmail.com>
2024-05-10 17:41:16 +0700

    feat(seo): add title and description

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: "Fylo Landing Page",
+  meta: [
+    {
+      name: "description",
+      content:
+        "This is a solution of Fylo Landing Page Challenge from Frontend Mentor by Devan",
+    },
+  ],
+});
+</script>
+
 <template>
   <LayoutHeader />
   <LayoutAbout />
