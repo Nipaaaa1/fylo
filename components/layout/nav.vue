feb3eb456796579565c3209fde0d5a0fe58e6e81
Nipaaaa1 <Frostone224@gmail.com>
2024-05-10 17:50:17 +0700

    styles(nav): add styles for desktop

--- components/layout/nav.vue
+++ components/layout/nav.vue
@@ -1,7 +1,9 @@
 <template>
-  <nav class="container flex items-center justify-between p-6 font-raleway">
+  <nav
+    class="container flex items-center justify-between p-6 font-raleway md:p-12"
+  >
     <NuxtImg class="h-full w-max" src="/images/logo.svg" alt="" />
-    <ul class="flex gap-4">
+    <ul class="flex gap-4 md:gap-8">
       <li><a href="#">Features</a></li>
       <li><a href="#">Team</a></li>
       <li><a href="#">Sign In</a></li>
