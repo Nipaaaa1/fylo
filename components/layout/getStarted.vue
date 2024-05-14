2f30fd485e7b0c1dd2ea115d1b2e1be287e504dc
Nipaaaa1 <Frostone224@gmail.com>
2024-05-14 16:43:48 +0700

    feat(getStarted): add email validation and hover effect

--- components/layout/getStarted.vue
+++ components/layout/getStarted.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+const email = ref<string>();
+const isEmail = ref<boolean>(true);
+
+const handleSubmit = (e: Event) => {
+  e.preventDefault();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(email.value as string)) {
+    console.log("berhasil");
+    isEmail.value = true;
+  } else {
+    console.log("gagal");
+    isEmail.value = false;
+  }
+};
+</script>
+
 <template>
   <div class="w-full bg-primary-desaturated-blue">
     <section
@@ -12,18 +29,25 @@
         </p>
       </div>
       <form
+        @submit="handleSubmit"
         class="mx-auto w-72 space-y-3 md:flex md:w-full md:flex-col md:space-y-5 md:pl-96"
       >
         <label for="email" class="sr-only w-0">Email</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="email@example.com"
-          class="w-full rounded p-2 placeholder:font-open-sans placeholder:text-sm"
-        />
+        <div class="w-full">
+          <input
+            v-model="email"
+            type="email"
+            name="email"
+            placeholder="email@example.com"
+            :class="`w-full rounded border p-2 text-primary-desaturated-blue placeholder:font-open-sans placeholder:text-sm ${isEmail ? '' : 'border-red-500'}`"
+          />
+          <p v-if="!isEmail" class="pt-1 text-left text-sm">
+            Please check your email
+          </p>
+        </div>
         <button
           type="submit"
-          class="w-full rounded bg-accent-blue p-2 text-sm font-bold tracking-wider text-white shadow-md md:w-max md:px-12 md:py-3"
+          class="w-full rounded bg-accent-blue p-2 text-sm font-bold tracking-wider text-white shadow-md transition ease-in-out hover:brightness-110 md:w-max md:px-12 md:py-3"
         >
           Get Started For Free
         </button>
